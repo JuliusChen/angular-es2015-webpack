a3fd7258d7e3b73d69fca31e43f6e265f3f43994
Julien Giovaresco <dev@giovaresco.fr>
2015-12-26 14:01:45 +0100

    Add a test file for RepoListController

---
+++ src/repo-list/repo-list.controller_test.js
@@ -0,0 +1,31 @@
+import Controller from './repo-list.controller.js';
+
+describe('Repo list controller', () => {
+
+	var $q, $rootScope;
+	var repoServiceStub;
+
+
+	beforeEach(angular.mock.inject((_$q_, _$rootScope_) => {
+		$q = _$q_;
+		$rootScope = _$rootScope_;
+	}));
+
+	beforeEach(() => {
+		repoServiceStub = sinon.stub({
+			githubRepositoriesOf: function() {}
+		});
+	});
+
+
+	it('should expose the repo list', () => {
+
+		repoServiceStub.githubRepositoriesOf.returns($q.when(['repo1', 'repo2']));
+
+		let controller = new Controller(repoServiceStub);
+		$rootScope.$apply();
+
+		expect(controller.repos).to.deep.equal(['repo1', 'repo2']);
+		expect(repoServiceStub.githubRepositoriesOf).to.be.calledWith('jgiovaresco')
+	});
+});
